c0192032b228449c2f306c9dec449f02bf826379
anhhunganh <hunganh020301@gmail.com>
2023-04-11 01:59:02 +0700

    update (product detail)

--- collection/product_detail.php
+++ collection/product_detail.php
@@ -27,6 +27,9 @@
         exit;
     }
     $each = mysqli_fetch_array($result);
+
+    $sql_sub_image = "select * from product_sub_images where product_id = '$id'";
+    $result_sub_image = mysqli_query($connect, $sql_sub_image);
     ?>
     <div class="main">
         <?php require '../pages/header.php' ?>
@@ -38,28 +41,31 @@
                             <div class="product-gallery">
                                 <img class="product-gallery__img" src="../admin/products/img/<?php echo $each['image'] ?>" alt="">
                                 <div class="product-gallery__list">
-                                    <div class="row">
-                                        <div class="col l-3">
-                                            <div class="product-gallery__item" onclick="changeImage('../admin/products/img/<?php echo $each['image'] ?> ')">
-                                                <img class="product-gallery__item-img" src="../admin/products/img/<?php echo $each['image'] ?>" alt="">
-                                            </div>
-                                        </div>
-                                        <div class="col l-3">
-                                            <div class="product-gallery__item" onclick="changeImage()">
-                                                <img class="product-gallery__item-img" src="../admin/products/img/<?php echo $each['image'] ?>" alt="">
-                                            </div>
-                                        </div>
-                                        <div class="col l-3">
-                                            <div class="product-gallery__item" onclick="changeImage()">
-                                                <img class="product-gallery__item-img" src="../admin/products/img/<?php echo $each['image'] ?>" alt="">
-                                            </div>
-                                        </div>
-                                        <div class="col l-3">
-                                            <div class="product-gallery__item" onclick="changeImage()">
-                                                <img class="product-gallery__item-img" src="../admin/products/img/<?php echo $each['image'] ?>" alt="">
-                                            </div>
-                                        </div>
+                                    <div class="product-gallery__item" onclick="changeImage('../admin/products/img/<?php echo $each['image'] ?>')">
+                                        <img class="product-gallery__item-img" src="../admin/products/img/<?php echo $each['image'] ?>" alt="">
                                     </div>
+                                    <button id="image_prev" class="sub-image__btn prev"><i class="fa-solid fa-chevron-left fa-beat fa-2xs"></i></button>
+                                    <button id="image_next" class="sub-image__btn next"><i class="fa-solid fa-chevron-right fa-beat fa-2xs"></i></button>
+                                    <?php foreach ($result_sub_image as $sub_image) { ?>
+                                        <div class="product-gallery__item" onclick="changeImage('../admin/products/sub_img/<?php echo $sub_image['source'] ?>')">
+                                            <img class="product-gallery__item-img" src="../admin/products/sub_img/<?php echo $sub_image['source'] ?>" alt="">
+                                        </div>
+                                    <?php } ?>
+                                    <!-- <div class="col l-3">
+                                            <div class="product-gallery__item" onclick="changeImage()">
+                                                <img class="product-gallery__item-img" src="../admin/products/img/<?php echo $each['image'] ?>" alt="">
+                                            </div>
+                                        </div>
+                                        <div class="col l-3">
+                                            <div class="product-gallery__item" onclick="changeImage()">
+                                                <img class="product-gallery__item-img" src="../admin/products/img/<?php echo $each['image'] ?>" alt="">
+                                            </div>
+                                        </div>
+                                        <div class="col l-3">
+                                            <div class="product-gallery__item" onclick="changeImage()">
+                                                <img class="product-gallery__item-img" src="../admin/products/img/<?php echo $each['image'] ?>" alt="">
+                                            </div>
+                                        </div> -->
                                 </div>
                             </div>
                         </div>
@@ -82,58 +88,112 @@
                                 </div>
                                 <form action="../cart/add_to_cart.php" method="post">
                                     <input type="hidden" name="id" value="<?php echo $each['id'] ?>">
-                                    <div class="product-size">
-                                        <label class="product-size__label">
-                                            <input type="radio" name="size" class="product-size__input" id="" value="36">
-                                            <span class="product-size__span">36</span>
-                                        </label>
-                                        <label class="product-size__label">
-                                            <input type="radio" name="size" class="product-size__input" id="" value="37">
-                                            <span class="product-size__span">37</span>
-                                        </label>
-                                        <label class="product-size__label">
-                                            <input type="radio" name="size" class="product-size__input" id="" value="38">
-                                            <span class="product-size__span">38</span>
-                                        </label>
-                                        <label class="product-size__label">
-                                            <input type="radio" name="size" class="product-size__input" id="" value="39">
-                                            <span class="product-size__span">39</span>
-                                        </label>
-                                        <label class="product-size__label">
-                                            <input type="radio" name="size" class="product-size__input" id="" value="40">
-                                            <span class="product-size__span">40</span>
-                                        </label>
-                                        <label class="product-size__label">
-                                            <input type="radio" name="size" class="product-size__input" id="" value="41">
-                                            <span class="product-size__span">41</span>
-                                        </label>
-                                        <label class="product-size__label">
-                                            <input type="radio" name="size" class="product-size__input" id="" value="42">
-                                            <span class="product-size__span">42</span>
-                                        </label>
-                                        <label class="product-size__label">
-                                            <input type="radio" name="size" class="product-size__input" id="" value="43">
-                                            <span class="product-size__span">43</span>
-                                        </label>
-                                        <label class="product-size__label">
-                                            <input type="radio" name="size" class="product-size__input" id="" value="44">
-                                            <span class="product-size__span">44</span>
-                                        </label>
+                                    <div class="product-size__wrap">
+                                        <div class="product-size__name">Size:</div>
+                                        <div class="product-size">
+                                            <label class="product-size__label">
+                                                <input type="radio" name="size" class="product-size__input" id="" value="36">
+                                                <span class="product-size__span">36</span>
+                                            </label>
+                                            <label class="product-size__label">
+                                                <input type="radio" name="size" class="product-size__input" id="" value="37">
+                                                <span class="product-size__span">37</span>
+                                            </label>
+                                            <label class="product-size__label">
+                                                <input type="radio" name="size" class="product-size__input" id="" value="38">
+                                                <span class="product-size__span">38</span>
+                                            </label>
+                                            <label class="product-size__label">
+                                                <input type="radio" name="size" class="product-size__input" id="" value="38.5">
+                                                <span class="product-size__span">38.5</span>
+                                            </label>
+                                            <label class="product-size__label">
+                                                <input type="radio" name="size" class="product-size__input" id="" value="39">
+                                                <span class="product-size__span">39</span>
+                                            </label>
+                                            <label class="product-size__label">
+                                                <input type="radio" name="size" class="product-size__input" id="" value="40">
+                                                <span class="product-size__span">40</span>
+                                            </label>
+                                            <label class="product-size__label">
+                                                <input type="radio" name="size" class="product-size__input" id="" value="40.5">
+                                                <span class="product-size__span">40.5</span>
+                                            </label>
+                                            <label class="product-size__label">
+                                                <input type="radio" name="size" class="product-size__input" id="" value="40.5">
+                                                <span class="product-size__span">40.5</span>
+                                            </label>
+                                            <label class="product-size__label">
+                                                <input type="radio" name="size" class="product-size__input" id="" value="41">
+                                                <span class="product-size__span">41</span>
+                                            </label>
+                                            <label class="product-size__label">
+                                                <input type="radio" name="size" class="product-size__input" id="" value="41.5">
+                                                <span class="product-size__span">41.5</span>
+                                            </label>
+                                            <label class="product-size__label">
+                                                <input type="radio" name="size" class="product-size__input" id="" value="42">
+                                                <span class="product-size__span">42</span>
+                                            </label>
+                                            <label class="product-size__label">
+                                                <input type="radio" name="size" class="product-size__input" id="" value="43">
+                                                <span class="product-size__span">43</span>
+                                            </label>
+                                            <label class="product-size__label">
+                                                <input type="radio" name="size" class="product-size__input" id="" value="44">
+                                                <span class="product-size__span">44</span>
+                                            </label>
+                                        </div>
                                     </div>
-                                        <button>Thêm vào giỏ hàng</button>
-                                    </form>
+                                    <button class="product-size__btn">Thêm vào giỏ hàng</button>
+                                </form>
                             </div>
                         </div>
+                    </div>
+                </div>
+                <div class="product-suggest">
+                    <?php
+                    $sql = "select * from products order by created_at desc limit 8";
+                    $result = mysqli_query($connect, $sql);
+                    ?>
+                    <div class="row">
+                        <hr class="col l-12">
+                        <h2 class="col l-12 product-suggest__heading text-center">
+                            Sản phẩm mới
+                        </h2>
+                        <?php foreach ($result as $each) { ?>
+                            <div class="col l-3">
+                                <div class="product-suggest__item">
+                                    <a href="./product_detail.php?id=<?php echo $each['id'] ?>" class="product-suggest__item-link">
+                                        <img src="../admin/products/img/<?php echo $each['image'] ?>" alt="" class="product-suggest__item-img">
+                                    </a>
+                                    <a href="./product_detail.php?id=<?php echo $each['id'] ?>" class="product-suggest__item-link">
+                                        <h4 class="product-suggest__item-name text-center"><?php echo $each['name'] ?></h4>
+                                    </a>
+                                    <span class="product-suggest__price text-center"><?php echo number_format($each['price'], 0, 0, '.') ?>₫</span>
+                                </div>
+                            </div>
+                        <?php } ?>
                     </div>
                 </div>
             </div>
 
         </div>
+        <?php require '../pages/footer.php' ?>
     </div>
 </body>
 <script>
     function changeImage(src) {
         document.querySelector('.product-gallery__img').src = src;
+    }
+
+    document.getElementById('image_next').onclick = function() {
+        let lists = document.querySelectorAll('.product-gallery__item');
+        document.querySelector('.product-gallery__list').appendChild(lists[0])
+    }
+    document.getElementById('image_prev').onclick = function() {
+        let lists = document.querySelectorAll('.product-gallery__item');
+        document.querySelector('.product-gallery__list').prepend(lists[lists.length - 1])
     }
 </script>
 
